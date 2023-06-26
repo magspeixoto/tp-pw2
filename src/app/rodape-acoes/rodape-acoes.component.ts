82ef105286238c65376a8d9a6f2310ce258f0404
afonsocampos13 <zeafonso_27@hotmail.com>
2023-06-26 20:24:37 +0100

    Afonso v2.3

--- src/app/rodape-acoes/rodape-acoes.component.ts
+++ src/app/rodape-acoes/rodape-acoes.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { APICallService } from '../services/apicall.service';
 
 @Component({
-  selector: 'app-root',
+  selector: 'rodape-acoes',
   templateUrl: './rodape-acoes.component.html',
   styleUrls: ['./rodape-acoes.component.css']
 })
 
 export class RodapeAcoesComponent {
   title = 'apidata'; 
+
   newdata:any;
+
   constructor(private _apiservice: APICallService){}
     
   ngOnInit(){
