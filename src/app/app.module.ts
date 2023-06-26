85b2c4283404fbf60ec6eb472a53a1db4ad1dbcc
afonsocampos13 <zeafonso_27@hotmail.com>
2023-06-26 11:08:10 +0100

    Afonso v1.5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,6 +29,7 @@ import { CarteiraAcoesComponent } from './carteira-acoes/carteira-acoes.componen
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     TooltipModule.forRoot(),
     BrowserAnimationsModule
--- src/app/carteira-acoes/carteira-acoes.component.ts
+++ src/app/carteira-acoes/carteira-acoes.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Acoes {
+            ticker: string;
+            empresa: string;
+            data: Date;
+            quantidade: number;
+            preco_unitario: number;
+}
 
 @Component({
   selector: 'app-carteira-acoes',
   templateUrl: './carteira-acoes.component.html',
   styleUrls: ['./carteira-acoes.component.css']
 })
-export class CarteiraAcoesComponent {
+export class CarteiraAcoesComponent implements OnInit{
+ acoes : Acoes [] = [];
 
+ constructor(private http: HttpClient) {}
+
+ ngOnInit(): void {
+  this.http
+    .get<Acoes[]>('./carteira_acoes.json')
+    .subscribe((data: Acoes[]) => {
+      this.acoes = data;
+    });
+}
 }
