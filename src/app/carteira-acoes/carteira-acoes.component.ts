9ec3d9817a6f01a2d32587e88490f796c56baa48
afonsocampos13 <zeafonso_27@hotmail.com>
2023-06-26 12:25:51 +0100

    Afonso v1.6

--- src/app/carteira-acoes/carteira-acoes.component.ts
+++ src/app/carteira-acoes/carteira-acoes.component.ts
@@ -15,15 +15,22 @@ interface Acoes {
   styleUrls: ['./carteira-acoes.component.css']
 })
 export class CarteiraAcoesComponent implements OnInit{
- acoes : Acoes [] = [];
+ acoes : Acoes[] = [];
 
  constructor(private http: HttpClient) {}
 
  ngOnInit(): void {
+  console.log('ngOnInit called'); // Add this line to check if the method is being called
+
   this.http
-    .get<Acoes[]>('./carteira_acoes.json')
+    .get<Acoes[]>('assets/carteira_acoes.json')
     .subscribe((data: Acoes[]) => {
       this.acoes = data;
+      console.log(this.acoes); // Add this line to check the received data
+    },
+    (error) => {
+      console.error('Error fetching data:', error);
     });
 }
+
 }
