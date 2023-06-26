769ac5ad136fad79b992cd7434e483089d2cefc0
magspeixoto <85014211+magspeixoto@users.noreply.github.com>
2023-06-26 18:19:30 +0100

    rod

--- src/app/carteira-acoes/carteira-acoes.component.ts
+++ src/app/carteira-acoes/carteira-acoes.component.ts
@@ -7,6 +7,8 @@ interface Acoes {
             data: Date;
             quantidade: number;
             preco_unitario: number;
+            api1: number;
+            api2: number;
 }
 
 @Component({
@@ -22,15 +24,26 @@ export class CarteiraAcoesComponent implements OnInit{
  ngOnInit(): void {
   console.log('ngOnInit called'); // Add this line to check if the method is being called
 
+  
+  
   this.http
     .get<Acoes[]>('assets/carteira_acoes.json')
     .subscribe((data: Acoes[]) => {
       this.acoes = data;
       console.log(this.acoes); // Add this line to check the received data
+
+      this.acoes.forEach((item: Acoes) => {
+        const url = `https://api.twelvedata.com/price?symbol=${item.ticker}&apikey=68d7747062ea44dcaf9b316179154992`;
+        this.http.get(url).subscribe((response: any) => {
+          const cotacao = parseFloat(response.price);
+          console.log(cotacao);
+          item.api1 = cotacao;
+        });
+      });
     },
     (error) => {
       console.error('Error fetching data:', error);
-    });
+    }
+  );
 }
-
 }
