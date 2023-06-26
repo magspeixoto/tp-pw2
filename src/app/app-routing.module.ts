b63a0a6ff72f6fcc982d04f59a0535dbe3c8c8d7
afonsocampos13 <zeafonso_27@hotmail.com>
2023-06-26 10:27:51 +0100

    Afonso

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
+import { ComprarAcoesComponent } from './comprar-acoes/comprar-acoes.component';
 
 const routes: Routes = [
-  { path: '', XX : XX},
-  { path: 'about', XX : XX},
+  { path:'pagina-inicial', component:PaginaInicialComponent},
+  { path:'comprar-acoes', component:ComprarAcoesComponent},
+  { path: '', redirectTo: 'pagina-inicial', pathMatch: 'full'},
+  { path: '**', redirectTo: 'pagina-inicial', pathMatch: 'full'},
 ];
 
 
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  constructor(
+    public router: Router
+    ) 
+    {
+    }
+
+
+
+  reloadPage(route: string) {
+    const currentRoute = this.router.url;
+    if (currentRoute === route) {
+      window.location.reload();
+    }
+  }
 }
