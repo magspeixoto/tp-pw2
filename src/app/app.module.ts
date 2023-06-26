d21e51fe69e70ca2bf9e1e8ef3f4ca79fe0f4235
magspeixoto <85014211+magspeixoto@users.noreply.github.com>
2023-06-26 15:33:05 +0100

    h 'origin/main'.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
-
-
 import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ComprarAcoesComponent } from './comprar-acoes/comprar-acoes.component';
 import { RodapeAcoesComponent } from './rodape-acoes/rodape-acoes.component';
-
-
 import { NovaCarteiraComponent } from './nova-carteira/nova-carteira.component';
 import { MyWatchlistComponent } from './my-watchlist/my-watchlist.component';
 import { CarteiraAcoesComponent } from './carteira-acoes/carteira-acoes.component';
@@ -29,7 +22,7 @@ import { CarteiraAcoesComponent } from './carteira-acoes/carteira-acoes.componen
     RodapeAcoesComponent,
     NovaCarteiraComponent,
     MyWatchlistComponent,
-    CarteiraAcoesComponent
+    CarteiraAcoesComponent,
   ],
   imports: [
     BrowserModule,
