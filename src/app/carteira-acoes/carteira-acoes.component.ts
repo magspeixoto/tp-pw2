d9621dbe42a921281a2bcf1a72720c5e460afb72
Ruben <rm74607@gmail.com>
2023-06-26 12:20:01 +0100

    ihkjh

--- src/app/carteira-acoes/carteira-acoes.component.ts
+++ src/app/carteira-acoes/carteira-acoes.component.ts
@@ -1,29 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-interface Acoes {
-            ticker: string;
-            empresa: string;
-            data: Date;
-            quantidade: number;
-            preco_unitario: number;
-}
-
 @Component({
   selector: 'app-carteira-acoes',
   templateUrl: './carteira-acoes.component.html',
   styleUrls: ['./carteira-acoes.component.css']
 })
 export class CarteiraAcoesComponent implements OnInit{
- acoes : Acoes [] = [];
 
- constructor(private http: HttpClient) {}
+  result: any;
+  urlToJson = 'assets/carteira_acoes.json';
 
- ngOnInit(): void {
-  this.http
-    .get<Acoes[]>('./carteira_acoes.json')
-    .subscribe((data: Acoes[]) => {
-      this.acoes = data;
+  constructor(public http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get<any>(this.urlToJson).subscribe(response => {
+      this.result = response;
     });
-}
+  }
+  
 }
