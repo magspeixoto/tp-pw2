752b3411d1f1485a4129741a9f2dac2334f2c0d8
magspeixoto <85014211+magspeixoto@users.noreply.github.com>
2023-06-26 18:33:15 +0100

    atual

--- src/app/carteira-acoes/carteira-acoes.component.ts
+++ src/app/carteira-acoes/carteira-acoes.component.ts
@@ -2,13 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 interface Acoes {
-            ticker: string;
-            empresa: string;
-            data: Date;
-            quantidade: number;
-            preco_unitario: number;
-            api1: number;
-            api2: number;
+  ticker: string;
+  empresa: string;
+  data: Date;
+  quantidade: number;
+  preco_unitario: number;
+  api1: number;
+  api2: number;
 }
 
 @Component({
@@ -16,34 +16,34 @@ interface Acoes {
   templateUrl: './carteira-acoes.component.html',
   styleUrls: ['./carteira-acoes.component.css']
 })
-export class CarteiraAcoesComponent implements OnInit{
- acoes : Acoes[] = [];
+export class CarteiraAcoesComponent implements OnInit {
+  acoes: Acoes[] = [];
 
- constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
- ngOnInit(): void {
-  console.log('ngOnInit called'); // Add this line to check if the method is being called
+  ngOnInit(): void {
+    console.log('ngOnInit called'); // Add this line to check if the method is being called
 
-  
-  
-  this.http
-    .get<Acoes[]>('assets/carteira_acoes.json')
-    .subscribe((data: Acoes[]) => {
-      this.acoes = data;
-      console.log(this.acoes); // Add this line to check the received data
+    this.http
+      .get<Acoes[]>('assets/carteira_acoes.json')
+      .subscribe((data: Acoes[]) => {
+        this.acoes = data;
+        console.log(this.acoes); // Add this line to check the received data
 
-      this.acoes.forEach((item: Acoes) => {
-        const url = `https://api.twelvedata.com/price?symbol=${item.ticker}&apikey=68d7747062ea44dcaf9b316179154992`;
-        this.http.get(url).subscribe((response: any) => {
-          const cotacao = parseFloat(response.price);
-          console.log(cotacao);
-          item.api1 = cotacao;
+        this.acoes.forEach((item: Acoes) => {
+          const url = `https://api.iex.cloud/v1/data/core/quote/${item.ticker}?token=sk_e511acd46d734053beec96a280678778`;
+          this.http.get(url).subscribe((response: any) => {
+            const cotacao = parseFloat(response[0].latestPrice);
+            const variacao = parseFloat(response[0].changePercent);
+            console.log(cotacao);
+            item.api1 = cotacao;
+            item.api2 = variacao;
+          });
         });
-      });
-    },
-    (error) => {
-      console.error('Error fetching data:', error);
-    }
-  );
-}
+      },
+      (error) => {
+        console.error('Error fetching data:', error);
+      }
+    );
+  }
 }
