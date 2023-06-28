33ff4b1f5505623257e4fed87b89bc987ed48ae7
afonsocampos13 <zeafonso_27@hotmail.com>
2023-06-28 14:11:40 +0100

    sdasdas

--- src/app/services/apicall.service.ts
+++ src/app/services/apicall.service.ts
@@ -10,7 +10,7 @@ export class APICallService {
 
   // Método para obter dados da API
   getRodape() {
-    // Realiza uma requisição HTTP GET para a API, passando o ticker como parâmetro
+    // Realiza uma requisição HTTP GET para a API
     return this._http.get<Top10>(
       `https://api.iex.cloud/v1/data/core/stock_collection/list?collectionName=mostactive&token=sk_e511acd46d734053beec96a280678778`
     );
