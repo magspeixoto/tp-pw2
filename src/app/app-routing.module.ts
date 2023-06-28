0f64f2e1c90f69dbf55766076ad67c9d0e75b1e7
afonsocampos13 <zeafonso_27@hotmail.com>
2023-06-28 23:13:39 +0100

    Afonso vFinal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
-import { ComprarAcoesComponent } from './comprar-acoes/comprar-acoes.component';
 import { CarteiraAcoesComponent } from './carteira-acoes/carteira-acoes.component';
 import { MyWatchlistComponent } from './my-watchlist/my-watchlist.component';
 
 const routes: Routes = [
   { path:'pagina-inicial', component:PaginaInicialComponent}, // Rota para a página inicial
-  { path:'comprar-acoes', component:ComprarAcoesComponent}, // Rota para a página de compra de ações
   { path:'carteira-acoes', component:CarteiraAcoesComponent}, // Rota para a página da carteira de ações
   { path:'my-watchlist', component:MyWatchlistComponent}, // Rota para a página da lista de acompanhamento
   { path: '', redirectTo: 'pagina-inicial', pathMatch: 'full'}, // Redireciona para a página inicial se nenhum caminho for especificado
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,23 +7,20 @@ import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
-import { ComprarAcoesComponent } from './comprar-acoes/comprar-acoes.component';
 import { RodapeAcoesComponent } from './rodape-acoes/rodape-acoes.component';
-import { NovaCarteiraComponent } from './nova-carteira/nova-carteira.component';
 import { MyWatchlistComponent } from './my-watchlist/my-watchlist.component';
 import { CarteiraAcoesComponent } from './carteira-acoes/carteira-acoes.component';
 import { CommonModule } from '@angular/common';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatIconModule } from '@angular/material/icon';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     PaginaInicialComponent,
     NavbarComponent,
-    ComprarAcoesComponent,
     RodapeAcoesComponent,
-    NovaCarteiraComponent,
     MyWatchlistComponent,
     CarteiraAcoesComponent
   ],
